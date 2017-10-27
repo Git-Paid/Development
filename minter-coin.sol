613c27d8e118a2bb2613c356956dd4540b837e5a
Aaron Anderson <aaronlynanderson@gmail.com>
2017-10-27 15:31:59 -0500

    Minter Coin functions on Rinkeby Testnet

---
+++ minter-coin.sol
@@ -0,0 +1,31 @@
+// This contract functions on Rinkeby
+
+pragma solidity ^0.4.0;
+
+contract MinterCoin {
+    // The keyword "public" makes those variables
+    // readable from outside.
+    address public minter;
+    mapping (address => uint) public balances;
+
+    // Events allow light clients to react on
+    // changes efficiently.
+    event Sent(address from, address to, uint amount);
+
+    // This is the constructor whose code is
+    // run only when the contract is created.
+
+    function MinterCoin() public {
+        minter = msg.sender;
+    }
+    function mint(address receiver, uint amount) public {
+        if (msg.sender != minter) return;
+        balances[receiver] += amount;
+    }
+    function send(address receiver, uint amount) public {
+        if (balances[msg.sender] < amount) return;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        Sent(msg.sender, receiver, amount);
+    }
+}
